181e1257b118078d4be3fc151a3c7ea2de3c831e
Svetlana Kudryavtseva <springinmypalms@gmail.com>
2021-05-30 05:49:47 +0300

    Home component and pass properties' date trough

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,26 @@
-export default function Home() {
-    return ( <
-        >
-        hello <
-        />
-    )
+import { sanityClient } from './sanity'
+const Home = ({ properties }) => {
+    console.log(properties)
+    return (`<div> </div>`)
 }
+
+export const getServerSideProps = async() => {
+    const query = `*[ _type == "property"]`
+    const properties = await sanityClient.fetch(query)
+
+    if (!properties.length) {
+        return {
+            props: {
+                properties: []
+            },
+        }
+    } else {
+        return {
+            props: {
+                properties
+            }
+        }
+    }
+}
+
+export default Home
